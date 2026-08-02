a4d24f8e648fab166aad9124c1d148c876e26b36
Error404ke <njenganjenga312@gmail.com>
2026-08-02 19:41:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ document.getElementById('preview-input')?.addEventListener('keypress', (e) => {
     }
 });
 
-// Smooth scrolling for navigation links
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
